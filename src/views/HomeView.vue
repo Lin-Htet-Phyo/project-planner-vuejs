a68b82a88438183ccebaf444d37eafcbe9f9cefb
Lin-Htet-Phyo <linhtethtetphyo181688@gmail.com>
2025-03-09 14:24:58 +0630

    Project Planner Commit

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,49 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Home</h1>
+    <div v-for="project in projects" :key="project.id"> 
+      <SingleProject :project="project" @delete="deleteProject" @complete="completeProject"  />
+    </div>
   </div>
+  <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
 </template>
-
 <script>
+import SingleProject from "../components/SingleProject";
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'HomeView',
+  name: "HomeView",
   components: {
-    HelloWorld
-  }
-}
+    SingleProject,
+  },
+  data() {
+    return {
+      projects: [],
+    };
+  },
+  mounted() {
+    fetch("http://localhost:3000/projects")
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        this.projects = data;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  },
+  methods: {
+    deleteProject(id) {
+      this.projects = this.projects.filter(project => {
+        return project.id != id;
+      });
+    },
+    completeProject(id) {
+      let findProject = this.projects.find((project) => {
+        return id === project.id;
+      });
+      findProject.complete = !findProject.complete;
+    },
+  },
+};
 </script>
