ff9af95cd4acfe451cdd4672af0fc6b736e4ecbd
Lin-Htet-Phyo <linhtethtetphyo181688@gmail.com>
2025-03-10 13:21:24 +0630

    add filter

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="home">
     <h1>Home</h1>
-    <div v-for="project in projects" :key="project.id"> 
+    <FilterNav @filterValue="current=$event" :current="current"></FilterNav>
+    <div v-for="project in filteredProjects" :key="project.id"> 
       <SingleProject :project="project" @delete="deleteProject" @complete="completeProject"  />
     </div>
   </div>
@@ -9,16 +10,35 @@
 </template>
 <script>
 import SingleProject from "../components/SingleProject";
+import FilterNav from "../components/FilterNav";
+
 // @ is an alias to /src
 export default {
   name: "HomeView",
   components: {
     SingleProject,
+    FilterNav,
   },
   data() {
     return {
       projects: [],
+      current: "all"
     };
+  },
+  computed: {
+    filteredProjects() {
+      if (this.current === 'complete') {
+        return this.projects.filter((project) => {
+          return project.complete;
+        });
+      }
+      if (this.current === 'ongoing') {
+        return this.projects.filter((project) => {
+          return !project.complete;
+        });
+      }
+      return this.projects;
+    }
   },
   mounted() {
     fetch("http://localhost:3000/projects")
